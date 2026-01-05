a4f2efdf783c1b1422f8808e049ade25a633cb47
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-05 19:49:14 +0100

    Add dotenv Gradle plugin and configure Maven credentials

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.3.0"
+    id("co.uzzu.dotenv.gradle") version "2.0.0"
     `maven-publish`
 }
 
@@ -53,6 +54,11 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+val maven_username = if (env.isPresent("MAVEN_USERNAME")) env.fetch("MAVEN_USERNAME") else ""
+val maven_password = if (env.isPresent("MAVEN_PASSWORD")) env.fetch("MAVEN_PASSWORD") else ""
+
 publishing {
     repositories {
         maven {
